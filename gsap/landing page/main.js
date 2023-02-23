db453c9608ec721770ecddd45e7d8e511b8c493a
Chris Vargas <gqcodes@gmail.com>
2023-02-23 11:09:25 -0500

    add animations to landing page

--- gsap/landing page/main.js
+++ gsap/landing page/main.js
@@ -1,17 +1,47 @@
 let tl = gsap.timeline();
 tl.fromTo(
-  '.logo',
+  '.info-section h1',
   {
-    x: -200,
+    x: 0,
+    y: 100,
     opacity: 0,
   },
   {
     x: 0,
+    y: 0,
     opacity: 1,
     delay: 0.5,
     duration: 1.3,
   }
 )
+  .fromTo(
+    '.info-section h4',
+    {
+      x: 0,
+      y: 100,
+      opacity: 0,
+    },
+    {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      delay: 0.5,
+      duration: 1.3,
+    }
+  )
+  .fromTo(
+    '.logo',
+    {
+      x: -200,
+      opacity: 0,
+    },
+    {
+      x: 0,
+      opacity: 1,
+      delay: 0.5,
+      duration: 1.3,
+    }
+  )
   .fromTo(
     '.menu',
     {
@@ -63,4 +93,54 @@ tl.fromTo(
       ease: 'power2.inOut',
     },
     '>-1'
+  )
+  .fromTo(
+    '.shape3',
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 0.6,
+      duration: 0.3,
+      ease: 'power2.inOut',
+    },
+    '>-1'
+  )
+  .fromTo(
+    '.shape3',
+    {
+      y: 300,
+    },
+    {
+      y: 270,
+      duration: 2,
+      repeat: -1,
+      yoyo: true,
+      ease: 'sine.inOut',
+    }
+  )
+  .fromTo(
+    '.shape2',
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 0.3,
+      duration: 0.3,
+      ease: 'power2.inOut',
+    },
+    '>-2'
+  )
+  .fromTo(
+    '.shape2',
+    {
+      y: -300,
+    },
+    {
+      y: -335,
+      duration: 2,
+      repeat: -1,
+      yoyo: true,
+      ease: 'sine.inOut',
+    }
   );
